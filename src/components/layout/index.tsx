35f617064ddef145dad10728f62323bea39f13dc
isisnaldojoao <isisnaldojoao@gmail.com>
2024-11-23 15:00:08 -0300

    merry christmas version

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../header';
 import { Menu } from '../menu';
+import Snowfall from 'react-snowfall';
 
 export function Layout(){
     return(
         <>
+            <Snowfall/>
             <Header/>
             <Menu/>
             <Outlet/>
