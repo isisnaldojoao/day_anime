d6358d661333074e4957517e4e235004bad87798
isisnaldojoao <isisnaldojoao@gmail.com>
2024-11-30 18:18:04 -0300

    tst version

--- src/pages/animes/index.tsx
+++ src/pages/animes/index.tsx
@@ -110,7 +110,7 @@ export function Anime (){
                     onClick={()=>animeTrailer(anime.attributes.canonicalTitle)}
                     >
                         <CirclePlay/>
-                        Assista online
+                        Assistir online
                     </button>
                 </div>
             </div>
