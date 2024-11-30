de8b0b80b94e18c1052c23bdc68c9c4755eb5f73
isisnaldojoao <isisnaldojoao@gmail.com>
2024-11-30 17:49:29 -0300

    production version for all visitants

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { Header } from '../header';
 import { Menu } from '../menu';
 import Snowfall from 'react-snowfall';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export function Layout(){
     return(
@@ -10,6 +11,7 @@ export function Layout(){
             <Header/>
             <Menu/>
             <Outlet/>
+            <SpeedInsights
         </>
     )
 }
