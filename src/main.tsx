42820fce0c58d22eee5b1f7eb5ac42585527b040
João Isisnaldo <145622770+isisnaldojoao@users.noreply.github.com>
2025-11-15 19:05:02 -0300

    fall version

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
-
-
+import Snowfall from 'react-snowfall';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <Snowfall style={{ zIndex: 0, position: 'fixed', top: 0, left: 0, pointerEvents: 'none' }}/>
     <Toaster />
     <App />
   </React.StrictMode>,
